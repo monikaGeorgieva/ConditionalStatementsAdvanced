615ff039a2ef5dab30ed99dacdddf059c723597f
monikaGeorgieva <88885355+monikaGeorgieva@users.noreply.github.com>
2023-08-01 14:40:56 +0100

    Update 2WeekendOrWorkingDay.js
    added comment

--- 2WeekendOrWorkingDay.js
+++ 2WeekendOrWorkingDay.js
@@ -1,11 +1,11 @@
 function solve(input){
     let day = input[0];
     switch (day) {
-        case "Monday":
+        case "Monday":    
         case "Tuesday":
         case "Wednesday":
         case "Thursday":
-        case "Friday": console.log("Working day");
+        case "Friday": console.log("Working day"); // the last valid case 
             break;
         case "Saturday":
         case "Sunday":console.log("Weekend");
